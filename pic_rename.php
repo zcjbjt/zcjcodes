a3121b38df7bb2f589653bf6cbc419e681ed0284
zz <zz@git.com>
2014-12-09 15:55:41 +0800

    'zc'

---
+++ pic_rename.php
@@ -0,0 +1,40 @@
+<?php
+	//strpos 这函数我擦尼玛
+
+die;
+	
+	define('SP', DIRECTORY_SEPARATOR);
+	echo "GoPhp\n";
+	$dir = dirname(__FILE__);  // c:\btl
+	$config_dir = array();
+
+	//rename( 'tempFile.txt', 'tempFile2.txt' );
+	foreach($config_dir as $val){
+		$tmp_dir = $dir . SP . $val;
+		$res = my_scandir($tmp_dir);
+		foreach($res as $valb){
+			$oldname = $tmp_dir . SP . $valb;
+			$newname = $tmp_dir . SP . $val . '-' .$valb;
+			rename( $oldname, $newname);
+			echo $oldname  . ' -> ' . $newname . "\n";				
+
+		}
+	}
+	
+	function my_scandir($dir){
+		$files = array();
+		if ( $handle = opendir($dir) ) {
+				while ( ($file = readdir($handle)) !== false ) {
+				if ( $file != ".." && $file != "." ) {
+				if ( is_dir($dir . "/" . $file) ) {
+					$files[$file] = scandir($dir . "/" . $file);
+				}else {
+					$files[] = $file;
+				}
+			}
+		}
+		closedir($handle);
+		return $files;
+		}
+	}
+?>
